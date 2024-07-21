cbe0c063e42b8ddfe1f6a715b43e9b1cf310f481
KaoneSeane <167741496+KaoneSeane@users.noreply.github.com>
2024-07-21 13:11:17 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "ba6e3eo126bbf24d2t80df9032f7aa6a";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
